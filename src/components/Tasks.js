252e9ad51d5b54ebb4da2bc83a479b5e0c1cc321
Rafael Garcia <rafaellevis07@gmail.com>
2022-12-29 18:19:14 -0800

    add list of tasks

---
+++ src/components/Tasks.js
@@ -0,0 +1,26 @@
+const tasks = [
+  {
+    id: 1,
+    text: "Change bed sheets",
+    day: "Jan 5th at 2:30pm",
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: "Clean the bathrooms",
+    day: "Jan 6th at 1:30pm",
+    reminder: true,
+  },
+  {
+    id: 1,
+    text: "Clean windows",
+    day: "Jan 5th at 2:30pm",
+    reminder: false,
+  },
+];
+
+const Tasks = () => {
+  return <div>Tasks</div>;
+};
+
+export default Tasks;
