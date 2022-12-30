e01e8d52b4a06d8983eaf9710fb69510e1eda9e3
Rafael Garcia <rafaellevis07@gmail.com>
2022-12-29 20:06:29 -0800

    hook useState

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,25 +1,27 @@
-const tasks = [
-  {
-    id: 1,
-    text: "Change bed sheets",
-    day: "Jan 5th at 2:30pm",
-    reminder: true,
-  },
-  {
-    id: 2,
-    text: "Clean the bathrooms",
-    day: "Jan 6th at 1:30pm",
-    reminder: true,
-  },
-  {
-    id: 3,
-    text: "Clean windows",
-    day: "Jan 5th at 2:30pm",
-    reminder: false,
-  },
-];
+import { useState } from "react";
 
 const Tasks = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Change bed sheets",
+      day: "Jan 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Clean the bathrooms",
+      day: "Jan 6th at 1:30pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Clean windows",
+      day: "Jan 5th at 2:30pm",
+      reminder: false,
+    },
+  ]);
+
   return (
     <>
       {tasks.map((task) => (
