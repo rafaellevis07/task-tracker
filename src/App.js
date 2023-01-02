cba14ee67f1c118c09fe96fc64513073484d2dc4
Rafael Garcia <rafaellevis07@gmail.com>
2023-01-02 14:07:36 -0800

    Toggle feature

--- src/App.js
+++ src/App.js
@@ -29,13 +29,22 @@ function App() {
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
+  // Toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
   return (
     <div className="container">
       <Header />
       {tasks.length > 0 ? (
-        <Tasks tasks={tasks} onDelete={deleteTask} />
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
-        "No tasks Show"
+        "No Tasks To Show"
       )}
     </div>
   );
