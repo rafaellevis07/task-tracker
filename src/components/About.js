2c8b87a285dd4661e9a471ddbd63288de94b0244
Rafael Garcia <rafaellevis07@gmail.com>
2023-01-03 22:39:25 -0800

    digital signature

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,7 @@ const About = () => {
   return (
     <div>
       <h4>Version 1.0.0</h4>
+      <a href="https://github.com/rafaellevis07"> By: Rafael Garcia</a> <br />
       <Link to="/"> Go Back</Link>
     </div>
   );
